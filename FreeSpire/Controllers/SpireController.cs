7920a38370bd0cc1e570104f1b298b63776c0d88
Ahmadjon <formyproject1999@gmail.com>
2022-06-25 16:50:06 +0500

    add template

--- FreeSpire/Controllers/SpireController.cs
+++ FreeSpire/Controllers/SpireController.cs
@@ -44,7 +44,48 @@ namespace FreeSpire.Controllers
             try
             {
                 var document = new Document();
-                document.LoadFromFile("Nemati.docx");
+                document.LoadFromFile("Nemati.dotx");
+
+                //var allTexts = StructureDocumentTags.GetAllTags(document);
+
+                var structureTagService = new StructureDocumentTags();
+                structureTagService.LoadAllTags(document);
+
+                // RegNumber
+                structureTagService.SetInlineTagValue("RegNumber", "Reg-1502");
+                structureTagService.SetTagValue("Context", "this is test");
+
+                var section = (document.Sections.FirstItem as Section);
+
+                //foreach(Table table in section)
+                //{
+                //    if(table.Title == "Footer")
+                //    {
+                //        foreach(TableCell cell in (table.Rows.FirstItem as TableRow).Cells)
+                //        {
+                //            var objectCell;
+                //            if((cell.FirstChild as StructureDocumentTag).SDTProperties.Tag == "RegionName")
+                //            {
+                //                //(cell.FirstChild as S)= "Наманган";
+
+                //            }
+
+                //            else if((cell.FirstChild as StructureDocumentTag).SDTProperties.Tag == "MainDate")
+                //            {
+                //                (cell.FirstChild as Paragraph).Text = DateTime.Now.ToString("D");
+                //            }
+                //        }
+                //    }
+                //}
+
+                var mainTable = section.Tables[0];
+                //var check_2 = mainTable.Rows[0].Cells[0].FirstChild as StructureDocumentTag;
+                ((mainTable.Rows[0].Cells[0].FirstChild as StructureDocumentTag).FirstChild as Paragraph).Text = "Наманган";
+                ((mainTable.Rows[0].Cells[1].FirstChild as StructureDocumentTag).FirstChild as Paragraph).Text = DateTime.Now.ToString("D");
+
+
+                document.SaveToFile("elif.docx");
+
                 return Ok("ok");
             }
             catch (Exception ex)
@@ -122,35 +163,144 @@ namespace FreeSpire.Controllers
             }
         }
 
-    //    [HttpGet]
-    //    public IActionResult Usta()
-    //    {
-    //        try
-    //        {
-    //            Workbook workbook = new Workbook();
-    //            Worksheet sheet = workbook.Worksheets[0];
-    //            sheet.Range["A1"].Text = "No";
-    //            sheet.Range["A1"].ColumnWidth = 10;
-    //            sheet.Range["A1"].Style.Font.IsBold = true;
-    //            sheet.Range["B1"].Text = "Кому";
-    //            sheet.Range["B1"].ColumnWidth = 35;
-    //            sheet.Range["C1"].Text = "Дата";
-    //            sheet.Range["C1"].ColumnWidth = 35;
-    //            for (int i = 0; i < 10; i++)
-    //            {
-    //                sheet.Range[$"A{i}"].Text = $"{i - 1}";
-    //                sheet.Range[$"A{i}"].ColumnWidth = 10;
-    //                sheet.Range[$"B{i}"].Text = string.Format("{0} polat", i + 1);
-    //                sheet.Range[$"B{i}"].ColumnWidth = 35;
-    //                sheet.Range[$"C{i}"].Text = $"{DateTime.UtcNow:dd-MM-yyyy HH:mm}";
-    //                sheet.Range[$"C{i}"].ColumnWidth = 35;
-    //                i++;
-    //            }
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            return BadRequest(ex.Message);
-    //        }
-    //    }
+
+        [NonAction]
+        public void Google()
+        {
+            using (Document document = new Document(@"F:\SDTsample.docx"))
+            {
+                StructureTags structureTags = GetAllTags(document);
+                List<StructureDocumentTagInline> tagInlines = structureTags.tagInlines;
+
+                for (int i = 0; i < tagInlines.Count; i++)
+                {
+                    string alias = tagInlines[i].SDTProperties.Alias;
+                    decimal id = tagInlines[i].SDTProperties.Id;
+                    string tag = tagInlines[i].SDTProperties.Tag;
+                    string STDType = tagInlines[i].SDTProperties.SDTType.ToString();
+
+                    //find the SDT which you want to fill accroding to it's type(e.g. SdtText, RichText), tag and so on.
+                    if (tagInlines[i].SDTProperties.SDTType == SdtType.RichText && tag == "tag")
+                    {
+                        //set the text
+                        tagInlines[i].OwnerParagraph.Text = "text";
+
+                    }
+                }
+                document.SaveToFile("13836.docx", FileFormat.Docx);
+                System.Diagnostics.Process.Start("13836.docx");
+
+            }
+        }
+
+        private StructureTags GetAllTags(Document document)
+        {
+            StructureTags structureTags = new StructureTags();
+            foreach (Section section in document.Sections)
+            {
+                foreach (DocumentObject obj in section.Body.ChildObjects)
+                {
+                    if (obj.DocumentObjectType == DocumentObjectType.Paragraph)
+                    {
+                        foreach (DocumentObject pobj in (obj as Paragraph).ChildObjects)
+                        {
+                            if (pobj.DocumentObjectType == DocumentObjectType.StructureDocumentTagInline)
+                            {
+                                structureTags.tagInlines.Add(pobj as StructureDocumentTagInline);
+                            }
+                        }
+                    }
+                    else if (obj.DocumentObjectType == DocumentObjectType.Table)
+                    {
+                        foreach (TableRow row in (obj as Table).Rows)
+                        {
+                            foreach (TableCell cell in row.Cells)
+                            {
+                                foreach (DocumentObject cellChild in cell.ChildObjects)
+                                {
+                                    if (cellChild.DocumentObjectType == DocumentObjectType.StructureDocumentTag)
+                                    {
+                                        structureTags.tags.Add(cellChild as StructureDocumentTag);
+                                    }
+                                    else if (cellChild.DocumentObjectType == DocumentObjectType.Paragraph)
+                                    {
+                                        foreach (DocumentObject pobj in (cellChild as Paragraph).ChildObjects)
+                                        {
+                                            if (pobj.DocumentObjectType == DocumentObjectType.StructureDocumentTagInline)
+                                            {
+                                                structureTags.tagInlines.Add(pobj as StructureDocumentTagInline);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return structureTags;
+        }
+
+        public class StructureTags
+        {
+            List<StructureDocumentTagInline> m_tagInlines;
+            public List<StructureDocumentTagInline> tagInlines
+            {
+                get
+                {
+                    if (m_tagInlines == null)
+                        m_tagInlines = new List<StructureDocumentTagInline>();
+                    return m_tagInlines;
+                }
+                set
+                {
+                    m_tagInlines = value;
+                }
+            }
+            List<StructureDocumentTag> m_tags;
+            public List<StructureDocumentTag> tags
+            {
+                get
+                {
+                    if (m_tags == null)
+                        m_tags = new List<StructureDocumentTag>();
+                    return m_tags;
+                }
+                set
+                {
+                    m_tags = value;
+                }
+            }
+        }
+            //    [HttpGet]
+            //    public IActionResult Usta()
+            //    {
+            //        try
+            //        {
+            //            Workbook workbook = new Workbook();
+            //            Worksheet sheet = workbook.Worksheets[0];
+            //            sheet.Range["A1"].Text = "No";
+            //            sheet.Range["A1"].ColumnWidth = 10;
+            //            sheet.Range["A1"].Style.Font.IsBold = true;
+            //            sheet.Range["B1"].Text = "Кому";
+            //            sheet.Range["B1"].ColumnWidth = 35;
+            //            sheet.Range["C1"].Text = "Дата";
+            //            sheet.Range["C1"].ColumnWidth = 35;
+            //            for (int i = 0; i < 10; i++)
+            //            {
+            //                sheet.Range[$"A{i}"].Text = $"{i - 1}";
+            //                sheet.Range[$"A{i}"].ColumnWidth = 10;
+            //                sheet.Range[$"B{i}"].Text = string.Format("{0} polat", i + 1);
+            //                sheet.Range[$"B{i}"].ColumnWidth = 35;
+            //                sheet.Range[$"C{i}"].Text = $"{DateTime.UtcNow:dd-MM-yyyy HH:mm}";
+            //                sheet.Range[$"C{i}"].ColumnWidth = 35;
+            //                i++;
+            //            }
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            return BadRequest(ex.Message);
+            //        }
+            //    }
+        }
     }
-}
---
+++ FreeSpire/StructureDocumentTags.cs
@@ -0,0 +1,181 @@
+﻿using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+
+namespace FreeSpire
+{
+    public class StructureDocumentTags
+    {
+        public List<StructureDocumentTagInline> TagInlines = new List<StructureDocumentTagInline>();
+        public List<StructureDocumentTag> Tags = new List<StructureDocumentTag>();
+
+        public void LoadAllTags(Document document)
+        {
+            foreach (Section section in document.Sections)
+            {
+                foreach (DocumentObject obj in section.Body.ChildObjects)
+                {
+                    GetSDTInDocumentObject(obj);
+                }
+            }
+        }
+
+        public void GetSDTInDocumentObject(DocumentObject documentObject)
+        {
+            if (documentObject.DocumentObjectType == DocumentObjectType.TextBox)
+            {
+                foreach (DocumentObject pobj in (documentObject as TextBox).ChildObjects)
+                {
+                    GetSDTInDocumentObject(pobj);
+                }
+            }
+
+            if (documentObject.DocumentObjectType == DocumentObjectType.Paragraph)
+            {
+                foreach (DocumentObject pobj in (documentObject as Paragraph).ChildObjects)
+                {
+                    if (pobj.DocumentObjectType == DocumentObjectType.StructureDocumentTagInline)
+                    {
+                        TagInlines.Add(pobj as StructureDocumentTagInline);
+                    }
+                    if (pobj.DocumentObjectType == DocumentObjectType.ShapeGroup)
+                    {
+                        foreach (DocumentObject pobjInner in pobj.ChildObjects)
+                        {
+                            GetSDTInDocumentObject(pobjInner);
+                        }
+                    }
+                    if (pobj.DocumentObjectType == DocumentObjectType.TextBox)
+                    {
+                        foreach (DocumentObject pobjInner in pobj.ChildObjects)
+                        {
+                            GetSDTInDocumentObject(pobjInner);
+                        }
+                    }
+                }
+            }
+            else if (documentObject is StructureDocumentTag)
+            {
+                Tags.Add(documentObject as StructureDocumentTag);
+            }
+        }
+
+        public string GetSDTText(string tagName)
+        {
+            string result = "";
+            try
+            {
+                if (string.IsNullOrEmpty(result))
+                {
+                    StructureDocumentTagInline inlineTag = GetTagInlines(tagName).FirstOrDefault();
+                    if (inlineTag != null)
+                    {
+                        result = inlineTag.SDTContent.Text;
+                    }
+                }
+
+                if (string.IsNullOrEmpty(result))
+                {
+                    StructureDocumentTag tag = GetTag(tagName);
+                    if (tag != null)
+                    {
+                        List<string> lines = new List<string>();
+                        foreach (Paragraph paragraph in tag.SDTContent.Paragraphs)
+                        {
+                            lines.Add(paragraph.Text);
+                        }
+                        result = String.Join(Environment.NewLine, lines.ToArray());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public List<TextRange> GetTagInlineTextRanges(string tagName)
+        {
+            List<TextRange> resultTextRanges = new List<TextRange>();
+            try
+            {
+                List<StructureDocumentTagInline> tagInlines = GetTagInlines(tagName);
+                if (tagInlines != null && tagInlines.Count > 0)
+                {
+                    foreach (StructureDocumentTagInline tagInline in tagInlines)
+                    {
+                        if (tagInline.ChildObjects.Count > 0 && (tagInline.ChildObjects[0] as TextRange) != null)
+                        {
+                            TextRange orginalTextRange = tagInline.ChildObjects[0] as TextRange;
+                            if (orginalTextRange != null)
+                            {
+                                orginalTextRange = orginalTextRange.Clone() as TextRange;
+                            }
+                            tagInline.ChildObjects.Clear();
+                            tagInline.ChildObjects.Add(orginalTextRange);
+                            resultTextRanges.Add(orginalTextRange);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return resultTextRanges;
+        }
+
+        public List<DocPicture> GetTagInlineDocPictures(string tagName)
+        {
+            List<DocPicture> resultDocPictures = new List<DocPicture>();
+            try
+            {
+                List<StructureDocumentTagInline> tagInlines = GetTagInlines(tagName);
+                if (tagInlines != null && tagInlines.Count > 0)
+                {
+                    foreach (StructureDocumentTagInline tagInline in tagInlines)
+                    {
+                        if (tagInline != null && tagInline.ChildObjects.Count > 0 && (tagInline.ChildObjects[0] as DocPicture != null))
+                        {
+                            resultDocPictures.Add(tagInline.ChildObjects[0] as DocPicture);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return resultDocPictures;
+        }
+
+        public List<StructureDocumentTagInline> GetTagInlines(string tagName)
+        {
+            return TagInlines.Where(s => s.SDTProperties.Tag.ToUpper() == tagName.ToUpper()).ToList();
+        }
+
+        public StructureDocumentTag GetTag(string tagName)
+        {
+            return Tags.FirstOrDefault(s => s.SDTProperties.Tag.ToUpper() == tagName.ToUpper());
+        }
+
+        public void SetTagValue(string tagName, string value)
+        {
+            var targetTag = Tags.FirstOrDefault(tag => tag.SDTProperties.Tag.ToLower() == tagName.ToLower());
+            if(targetTag != null)
+            {
+                (targetTag.FirstChild as Paragraph).Text = value;
+            }
+        }
+
+        public void SetInlineTagValue(string inlineTagName, string value)
+        {
+            var inlineTag = TagInlines.FirstOrDefault(l=>l.SDTProperties.Tag.ToLower() == inlineTagName.ToLower());
+            if(inlineTag != null)
+            {
+                (inlineTag.FirstChild as TextRange).Text = value;
+            }
+        }
+    }
+}
